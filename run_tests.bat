1bf634b5b4bfbe993dae2f8e2e506fa33e7e74d3
Ron <ron@gmail.com>
2017-06-10 06:25:55 -0700

    Added Windows stuff

---
+++ run_tests.bat
@@ -0,0 +1,37 @@
+ECHO OFF
+
+.\Debug\sch.exe DRR .\tests\inp1.txt out.txt 1 2
+fc /W out.txt ./tests/out1_DRR_w1_q2.txt
+
+.\Debug\sch.exe DRR .\tests\inp1.txt out.txt 3 2
+fc /W out.txt ./tests/out1_DRR_w3_q2.txt
+
+.\Debug\sch.exe DRR .\tests\inp2.txt out.txt 1 10
+fc /W out.txt ./tests/out2_DRR_w1_q10.txt
+
+.\Debug\sch.exe DRR .\tests\inp2.txt out.txt 8 10
+fc /W out.txt ./tests/out2_DRR_w8_q10.txt
+
+.\Debug\sch.exe DRR .\tests\inp3.txt out.txt 1 40
+fc /W out.txt ./tests/out3_DRR_w1_q40.txt
+
+.\Debug\sch.exe DRR .\tests\inp3.txt out.txt 30 40
+fc /W out.txt ./tests/out3_DRR_w30_q40.txt
+
+.\Debug\sch.exe RR .\tests\inp1.txt out.txt 1 0
+fc /W out.txt ./tests/out1_WRR_w1_q0.txt
+
+.\Debug\sch.exe RR .\tests\inp1.txt out.txt 3 0
+fc /W out.txt ./tests/out1_WRR_w3_q0.txt
+
+.\Debug\sch.exe RR .\tests\inp2.txt out.txt 1 0
+fc /W out.txt ./tests/out2_WRR_w1_q0.txt
+
+.\Debug\sch.exe RR .\tests\inp2.txt out.txt 8 0
+fc /W out.txt ./tests/out2_WRR_w8_q0.txt
+
+.\Debug\sch.exe RR .\tests\inp3.txt out.txt 1 0
+fc /W out.txt ./tests/out3_WRR_w1_q0.txt
+
+.\Debug\sch.exe RR .\tests\inp3.txt out.txt 30 0
+fc /W out.txt ./tests/out3_WRR_w30_q0.txt
